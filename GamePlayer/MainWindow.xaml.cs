bb0ff93548beed447dd7f8b7fc66a07e0bdd2e58
vfridell <gusknows@gmail.com>
2019-06-05 16:35:16 -0400

    minor ui improvement

--- GamePlayer/MainWindow.xaml.cs
+++ GamePlayer/MainWindow.xaml.cs
@@ -22,27 +22,27 @@ namespace GamePlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public Game Game { get; set; }
+
         public MainWindow()
         {
-            InitializeComponent();
-            Point[] arrowPoints = { Point.Get(2, 9), Point.Get(4, 9), Point.Get(3, 8), Point.Get(4, 8), Point.Get(2, 7), Point.Get(3, 7), Point.Get(5, 7), Point.Get(6, 7), Point.Get(7, 7), Point.Get(8, 7), Point.Get(1, 6), Point.Get(2, 6), Point.Get(4, 6), Point.Get(8, 6), Point.Get(0, 5), Point.Get(5, 5), Point.Get(1, 4), Point.Get(2, 4), Point.Get(5, 4), Point.Get(6, 4), Point.Get(7, 4), Point.Get(7, 3), Point.Get(1, 2), Point.Get(9, 2), Point.Get(0, 0), Point.Get(7, 0), };
-            Dictionary<Point, Amazon> amazonsDict = new Dictionary<Point, Amazon> {
-                { Point.Get(2,8), AmazonPlayer1.Get() }, { Point.Get(6,6), AmazonPlayer1.Get() }, { Point.Get(3,3), AmazonPlayer1.Get() }, { Point.Get(7,2), AmazonPlayer1.Get() },
-                { Point.Get(3,9), AmazonPlayer2.Get() }, { Point.Get(4,7), AmazonPlayer2.Get() }, { Point.Get(3,1), AmazonPlayer2.Get() }, { Point.Get(6,1), AmazonPlayer2.Get() }
-            };
-            PieceGrid grid = new PieceGrid(10, amazonsDict);
-            foreach (Point p in arrowPoints)
-            {
-                grid.PointPieces[p] = ArrowPlayer1.Get();
-            }
 
+        }
+
+        private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
             Game game = new Game();
             game.Begin(null, null, 10);
-            //game.CurrentBoard.PieceGrid = grid;
-
-            AmazonBoardControl boardControl = new AmazonBoardControl(game.CurrentBoard, true);
+            AmazonBoardControl boardControl = new AmazonBoardControl(game.CurrentBoard, false);
             MainGrid.Children.Add(boardControl);
-
+            MainGrid.UpdateLayout();
         }
     }
 }
