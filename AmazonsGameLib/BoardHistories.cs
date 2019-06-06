5dfd2de0d46bf2cf3b58bd77f7c3535824df4210
vfridell <gusknows@gmail.com>
2019-06-06 19:13:13 -0400

    minor board reversal stuff

---
+++ AmazonsGameLib/BoardHistories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonsGameLib
+{
+    public class BoardHistories
+    {
+        public Board Board { get; set; }
+
+        public BoardHistories(Board board)
+        {
+            Board = board;
+        }
+
+        public IEnumerable<Board> GetPreviousBoards()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
